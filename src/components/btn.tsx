e50f1392768481e922743d865af5e02d60305183
Archer <53667566+thearcherr@users.noreply.github.com>
2025-02-24 18:02:30 +0500

    production error fix, remove unused vars

--- src/components/btn.tsx
+++ src/components/btn.tsx
@@ -3,7 +3,6 @@
 import { motion } from "motion/react";
 import React, { JSX, useState } from "react";
 import Type_Button from "@/types/typebutton";
-import { animate } from "motion";
 
 export default function Button({
   color,
@@ -11,20 +10,6 @@ export default function Button({
   rounded,
   padding,
 }: Type_Button): JSX.Element {
-  const variants = {
-    initial: {
-      opacity: 0,
-      y: 10,
-    },
-
-    animate: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.25,
-      },
-    },
-  };
 
   const [isVisible, setVisible] = useState(false);
 
@@ -32,7 +17,7 @@ export default function Button({
     <motion.button
       onMouseEnter={() => setVisible(true)}
       onMouseLeave={() => setVisible(false)}
-      className={`relative overflow-hidden border hover:border-black border-${color} p-2 font-${weight} text-${color} ${rounded ? "rounded-md" : ""}`}
+      className={`relative overflow-hidden border hover:border-black border-${color} p-${padding ? padding : "2"} font-${weight} text-${color} ${rounded ? "rounded-md" : ""}`}
     >
       <motion.div
         className="absolute inset-0 z-0 bg-black bg-opacity-70"
